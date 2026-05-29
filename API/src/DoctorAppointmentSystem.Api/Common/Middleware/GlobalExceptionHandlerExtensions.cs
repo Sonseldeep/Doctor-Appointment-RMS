608b50961dab4487ef73a459aff860e9cf86305e
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-05-29 16:13:10 +0545

    add global execption middleware

---
+++ API/src/DoctorAppointmentSystem.Api/Common/Middleware/GlobalExceptionHandlerExtensions.cs
@@ -0,0 +1,11 @@
+﻿using DoctorAppointmentSystem.Common.Middleware;
+
+namespace DoctorAppointmentSystem.Api.Common.Middleware;
+
+public static class GlobalExceptionHandlerExtensions
+{
+    public static IApplicationBuilder UseGlobalExceptionHandler(this IApplicationBuilder app)
+    {
+        return app.UseMiddleware<GlobalExceptionHandlerMiddleware>();
+    }
+}
---
+++ API/src/DoctorAppointmentSystem.Api/Common/Middleware/GlobalExceptionHandlerMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Text.Json;
+using DoctorAppointmentSystem.Api.Common;
+using DoctorAppointmentSystem.Api.Common.Exceptions;
+
+namespace DoctorAppointmentSystem.Common.Middleware;
+
+public class GlobalExceptionHandlerMiddleware(
+    RequestDelegate next,
+    ILogger<GlobalExceptionHandlerMiddleware> logger)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, ex.Message);
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private static async Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+
+        var (statusCode, message) = exception switch
+        {
+            // custom domain exception (you can add later)
+            AppException appEx => (appEx.StatusCode, appEx.Message),
+
+            _ => (StatusCodes.Status500InternalServerError, "An unexpected error occurred")
+        };
+
+        var response = ApiResponse<object>.ErrorResponse(message, (HttpStatusCode)statusCode);
+
+        context.Response.StatusCode = statusCode;
+
+        var json = JsonSerializer.Serialize(response, new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        });
+
+        await context.Response.WriteAsync(json);
+    }
+}
