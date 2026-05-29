e91673f39323d976e77c9f797ef62235705d3be6
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-05-29 16:15:26 +0545

    add DI for infrastructure

---
+++ API/src/DoctorAppointmentSystem.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using DoctorAppointmentSystem.Application.Abstractions.Data;
+using DoctorAppointmentSystem.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DoctorAppointmentSystem.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IUnitOfWork>(serviceProvider => serviceProvider.GetRequiredService<ApplicationDbContext>());
+
+        var connectionString = configuration.GetConnectionString("Database") ??
+                               throw new ArgumentNullException(nameof(configuration));
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseNpgsql(connectionString).UseCamelCaseNamingConvention();
+        });
+           
+        return services;
+    }
+}
