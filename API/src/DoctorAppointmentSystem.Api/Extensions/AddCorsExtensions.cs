2ef21a26555a4ee8dc00af8cfacffd7efa79befc
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-05-29 16:08:58 +0545

    add CORS extension

---
+++ API/src/DoctorAppointmentSystem.Api/Extensions/AddCorsExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace DoctorAppointmentSystem.Api.Extensions;
+
+public static class AddCorsExtensions
+{
+    public static IServiceCollection AddCorsCollection(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowSpecificOrigin",
+                corsPolicyBuilder =>
+                {
+                    corsPolicyBuilder.WithOrigins("http://localhost:5173")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials()
+                        .SetPreflightMaxAge(TimeSpan.FromMinutes(10))
+                        .SetIsOriginAllowedToAllowWildcardSubdomains();
+                });
+        });
+        return services;
+    }
+}
