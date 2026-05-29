3a235a16e5662c838b444d1db4db2ba7af651e90
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-05-29 16:09:49 +0545

    add Genereic API Response wrapper

---
+++ API/src/DoctorAppointmentSystem.Api/Common/ApiResponse.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+
+namespace DoctorAppointmentSystem.Api.Common;
+
+public class ApiResponse<T>
+{
+    public bool IsSuccess { get; set; }
+    public HttpStatusCode StatusCode { get; set; }
+    public T? Data { get; set; }
+    public string? Message { get; set; }
+    public List<string> Errors { get; set; } = [];
+
+    public static ApiResponse<T> SuccessResponse(T? data, string? message = null,
+        HttpStatusCode statusCode = HttpStatusCode.OK)
+    {
+        return new ApiResponse<T>
+        {
+            IsSuccess = true,
+            StatusCode = statusCode,
+            Data = data,
+            Message = message
+        };
+    }
+
+    public static ApiResponse<T> ErrorResponse(string error,
+        HttpStatusCode statusCode = HttpStatusCode.BadRequest)
+    {
+        return new ApiResponse<T>
+        {
+            IsSuccess = false,
+            StatusCode = statusCode,
+            Message = error,
+            Errors = [error]
+        };
+    }
+}
