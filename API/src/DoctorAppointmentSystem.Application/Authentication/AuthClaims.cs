bf4aac2fe311779f7343c449edb91a41a0c7273f
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-06-01 16:37:09 +0545

    feat: add AuthClaims class for creating user claims

---
+++ API/src/DoctorAppointmentSystem.Application/Authentication/AuthClaims.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Claims;
+using DoctorAppointmentSystem.Domain.Users;
+
+namespace DoctorAppointmentSystem.Application.Authentication;
+
+internal static class AuthClaims
+{
+    public static IReadOnlyCollection<Claim> Create(User user)
+    {
+        Claim[] claims =
+        [
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Email, user.Email),
+            new Claim(ClaimTypes.Role, user.Role.ToString())
+        ];
+
+        return claims;
+    }
+}
