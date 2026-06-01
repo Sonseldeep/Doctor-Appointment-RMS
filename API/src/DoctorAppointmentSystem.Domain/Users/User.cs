3540399a4da705b782d58465079d156fda8e31c0
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-06-01 16:39:16 +0545

    feat: add User entity and configuration for database mapping

---
+++ API/src/DoctorAppointmentSystem.Domain/Users/User.cs
@@ -0,0 +1,44 @@
+﻿using DoctorAppointmentSystem.Domain.Abstractions;
+
+namespace DoctorAppointmentSystem.Domain.Users;
+
+public sealed class User : Entity
+{
+    private User()
+    {
+    }
+
+    public User(
+        Guid id,
+        string firstName,
+        string lastName,
+        string email, 
+        string passwordHash,
+        UserRole role)
+        : base(id)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        PasswordHash = passwordHash;
+        Role = role;
+    }
+
+    public string FirstName { get; private set; } = string.Empty;
+
+    public string LastName { get; private set; } = string.Empty;
+
+    public string Email { get; private set; } = string.Empty;
+
+    public string PasswordHash { get; private set; } = string.Empty;
+
+    public UserRole Role { get; private set; }
+
+    public static User Create(string firstName, string lastName, string email, string passwordHash, UserRole role)
+    {
+        var user = new User(Guid.NewGuid(), firstName, lastName, email, passwordHash, role);
+        return user;
+    }
+    
+    
+}
