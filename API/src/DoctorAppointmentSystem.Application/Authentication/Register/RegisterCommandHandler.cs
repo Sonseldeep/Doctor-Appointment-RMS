7eca94959b813e74aad76c3669ee45099521079a
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-06-02 11:57:09 +0545

    remove the AuthResponse for register

--- API/src/DoctorAppointmentSystem.Application/Authentication/Register/RegisterCommandHandler.cs
+++ API/src/DoctorAppointmentSystem.Application/Authentication/Register/RegisterCommandHandler.cs
@@ -9,22 +9,20 @@ using ErrorOr;
 namespace DoctorAppointmentSystem.Application.Authentication.Register;
 
 public class RegisterCommandHandler : 
-    ICommandHandler<RegisterCommand, AuthResponse>
+    ICommandHandler<RegisterCommand>
 {
     private readonly IUserRepository _userRepository;
     private readonly IPasswordHasher _passwordHasher;
-    private readonly IJwtTokenGenerator _jwtTokenGenerator;
     private readonly IUnitOfWork _unitOfWork;
 
-    public RegisterCommandHandler(IUserRepository userRepository, IPasswordHasher passwordHasher, IJwtTokenGenerator jwtTokenGenerator, IUnitOfWork unitOfWork)
+    public RegisterCommandHandler(IUserRepository userRepository, IPasswordHasher passwordHasher, IUnitOfWork unitOfWork)
     {
         _userRepository = userRepository;
         _passwordHasher = passwordHasher;
-        _jwtTokenGenerator = jwtTokenGenerator;
         _unitOfWork = unitOfWork;
     }
 
-    public async Task<ErrorOr<AuthResponse>> Handle(RegisterCommand request, CancellationToken cancellationToken)
+    public async Task<ErrorOr<Success>> Handle(RegisterCommand request, CancellationToken cancellationToken)
     {
        var existingUser = await _userRepository.GetByEmailAsync(request.Email, cancellationToken);
        if (existingUser is not null)
@@ -44,12 +42,10 @@ public class RegisterCommandHandler :
        
        await _userRepository.AddAsync(user, cancellationToken);
        await _unitOfWork.SaveChangesAsync(cancellationToken);
+       
+       return Result.Success;
 
-       var claims = AuthClaims.Create(user);
 
-       var accessToken = _jwtTokenGenerator.GenerateAccessToken(claims);
-       var response = new AuthResponse(accessToken);
-       return response;
     }
     
     private static UserRole ParseRole(string role)
