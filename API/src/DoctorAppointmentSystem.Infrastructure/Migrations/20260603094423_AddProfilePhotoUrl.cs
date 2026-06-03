146c49e3a35db57aac0e47d780902095db1c29c9
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-06-03 16:05:35 +0545

    m

---
+++ API/src/DoctorAppointmentSystem.Infrastructure/Migrations/20260603094423_AddProfilePhotoUrl.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoctorAppointmentSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProfilePhotoUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsEmailVerified",
+                schema: "hospital_management",
+                table: "users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilePhotoUrl",
+                schema: "hospital_management",
+                table: "users",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePhotoUrl",
+                schema: "hospital_management",
+                table: "users");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsEmailVerified",
+                schema: "hospital_management",
+                table: "users",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: false);
+        }
+    }
+}
