bf5c006505fe21a4d0e7b030141c6a7a4653fd77
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-06-03 16:00:49 +0545

    add modal for file upload

---
+++ API/src/DoctorAppointmentSystem.Application/Abstractions/Storage/FileUploadModel.cs
@@ -0,0 +1,7 @@
+﻿namespace DoctorAppointmentSystem.Application.Abstractions.Storage;
+
+public sealed record FileUploadModel(
+    Stream Content,
+    string FileName,
+    string ContentType,
+    long SizeInBytes);
