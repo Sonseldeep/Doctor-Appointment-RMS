c43d552f6a9c53d35242618c1621509a9e7d25a8
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-06-04 16:06:41 +0545

    add validation rule for patient profile update

---
+++ API/src/DoctorAppointmentSystem.Application/Features/Patients/UpdatePatientProfile/UpdatePatientProfileCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace DoctorAppointmentSystem.Application.Features.Patients.UpdatePatientProfile;
+
+internal sealed class UpdatePatientProfileCommandValidator
+    : AbstractValidator<UpdatePatientProfileCommand>
+{
+    public UpdatePatientProfileCommandValidator()
+    {
+        RuleFor(x => x.UserId).NotEmpty();
+
+        RuleFor(x => x.PhoneNumber)
+            .MaximumLength(30);
+
+        RuleFor(x => x.Address)
+            .MaximumLength(500);
+    }
+}
