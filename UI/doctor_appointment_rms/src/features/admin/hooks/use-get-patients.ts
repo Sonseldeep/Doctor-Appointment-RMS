c8ee6658f8303a156af8777b53d0a89bf407712e
PratikChandB19 <pratikchandb19@gmail.com>
2026-06-09 16:30:52 +0545

    Get Patient for Admin Integration

---
+++ UI/doctor_appointment_rms/src/features/admin/hooks/use-get-patients.ts
@@ -0,0 +1,11 @@
+import { useQuery } from "@tanstack/react-query";
+import { getPatients } from "../api/get-patients";
+import { PatientFilterParams } from "../types";
+
+export const useGetPatients = (params: PatientFilterParams) => {
+  return useQuery({
+    queryKey: ["admin", "patients", params],
+    queryFn: () => getPatients(params),
+    placeholderData: (previousData) => previousData, // Prevents layout flickering during pagination
+  });
+};
