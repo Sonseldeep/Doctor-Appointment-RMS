a2c811f250f79fa3afbc5a9fbf0e99731155ff24
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-06-11 16:14:56 +0545

    defined notification hub for users

---
+++ API/src/DoctorAppointmentSystem.Infrastructure/Hubs/NotificationHub.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace DoctorAppointmentSystem.Infrastructure.Hubs;
+
+[Authorize]
+public sealed class NotificationHub : Hub
+{
+ 
+    public override async Task OnConnectedAsync()
+    {
+        var userId = Context.UserIdentifier;
+
+        if (userId is not null)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, GetGroupName(userId));
+        }
+
+        await base.OnConnectedAsync();
+    }
+
+    
+    public override async Task OnDisconnectedAsync(Exception? exception)
+    {
+        var userId = Context.UserIdentifier;
+
+        if (userId is not null)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, GetGroupName(userId));
+        }
+
+        await base.OnDisconnectedAsync(exception);
+    }
+
+    public static string GetGroupName(string userId) => $"user-{userId}";
+}
