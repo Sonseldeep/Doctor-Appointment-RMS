caab6a9c4dcbfa865d1ce62f25537bcd98f21a84
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-06-12 13:16:07 +0545

    add remove rating by users command

---
+++ API/src/DoctorAppointmentSystem.Application/Features/Ratings/DeleteRating/DeleteRatingCommand.cs
@@ -0,0 +1,7 @@
+﻿using DoctorAppointmentSystem.Application.Abstractions.Messaging;
+
+namespace DoctorAppointmentSystem.Application.Features.Ratings.DeleteRating;
+
+public sealed record DeleteRatingCommand(
+    Guid PatientUserId,
+    Guid RatingId) : ICommand;
