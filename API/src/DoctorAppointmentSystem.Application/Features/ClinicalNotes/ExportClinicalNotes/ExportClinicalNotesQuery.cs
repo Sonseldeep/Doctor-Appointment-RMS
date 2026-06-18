fa2d0f4f28b68c6de4e09681f41ad57cf6d5e8df
PratikChandB19 <pratikchandb19@gmail.com>
2026-06-18 16:48:38 +0545

    Export Pdf for prescription added

---
+++ API/src/DoctorAppointmentSystem.Application/Features/ClinicalNotes/ExportClinicalNotes/ExportClinicalNotesQuery.cs
@@ -0,0 +1,6 @@
+﻿using DoctorAppointmentSystem.Application.Abstractions.Messaging;
+
+namespace DoctorAppointmentSystem.Application.Features.ClinicalNotes.ExportClinicalNotes;
+
+// CHANGED: Query now takes the specific appointmentId instead of a broad patientId
+public record ExportClinicalNotesQuery(Guid AppointmentId) : IQuery<byte[]>;
