e58b04d977f8569ce086081918e2c66d5533ce67
PratikChandB19 <pratikchandb19@gmail.com>
2026-06-19 14:09:32 +0545

    Follow-ups feature added

---
+++ API/src/DoctorAppointmentSystem.Application/Features/ClinicalNotes/GetUpcomingFollowUp/GetUpcomingFollowUpsQuery.cs
@@ -0,0 +1,6 @@
+﻿using DoctorAppointmentSystem.Application.Abstractions.Messaging;
+using DoctorAppointmentSystem.Application.Features.ClinicalNotes.Contracts;
+
+namespace DoctorAppointmentSystem.Application.Features.ClinicalNotes.GetUpcomingFollowUps;
+
+public sealed record GetUpcomingFollowUpsQuery(Guid UserId) : IQuery<IReadOnlyList<ClinicalNoteResponse>>;
