b4b0d55e6f3f72d9e21d3cb78ca169bc116b2d88
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-06-30 14:33:54 +0545

    refactor: the my appointment in pagination behavior

--- API/src/DoctorAppointmentSystem.Infrastructure/Repositories/AppointmentRepository.cs
+++ API/src/DoctorAppointmentSystem.Infrastructure/Repositories/AppointmentRepository.cs
@@ -1,4 +1,5 @@
-﻿using DoctorAppointmentSystem.Application.Abstractions.Appointments;
+﻿using System.Linq.Expressions;
+using DoctorAppointmentSystem.Application.Abstractions.Appointments;
 using DoctorAppointmentSystem.Application.Common;
 using DoctorAppointmentSystem.Domain.Appointments;
 using DoctorAppointmentSystem.Domain.Patients;
@@ -62,65 +63,38 @@ internal sealed class AppointmentRepository : IAppointmentRepository
                 cancellationToken);
     }
 
-    public async Task<PagedResult<AppointmentWithDetailsDto>> GetForPatientWithDetailsAsync(
+    public Task<PagedResult<AppointmentWithDetailsDto>> GetForPatientWithDetailsAsync(
         Guid patientUserId,
         int page,
         int pageSize,
         CancellationToken cancellationToken)
-    {
-        var today = DateOnly.FromDateTime(DateTime.UtcNow);
+        => GetWithDetailsAsync(a => a.PatientUserId == patientUserId, page, pageSize, cancellationToken);
 
-        // 1. Get the total count
-        var baseQuery = _db.Appointments.Where(a => a.PatientUserId == patientUserId);
-        var totalCount = await baseQuery.CountAsync(cancellationToken);
-
-        // 2. Perform the Joins
-        var query = baseQuery
-            .AsNoTracking()
-            .Join(_db.Users.AsNoTracking(), a => a.DoctorUserId, u => u.Id, (a, du) => new { a, du })
-            .Join(_db.DoctorProfiles.AsNoTracking(), x => x.a.DoctorUserId, dp => dp.UserId, (x, dp) => new { x.a, x.du, dp })
-            .Join(_db.Users.AsNoTracking(), x => x.a.PatientUserId, pu => pu.Id, (x, pu) => new { x.a, x.du, x.dp, pu })
-            .LeftJoin(_db.PatientProfiles.AsNoTracking(), x => x.a.PatientUserId, pp => pp.UserId, (x, pp) => new { x.a, x.du, x.dp, x.pu, pp });
-
-        // 3. Paginate and Select
-        var items = await query
-            .OrderByDescending(x => x.a.StartUtc)
-            .Skip((page - 1) * pageSize)
-            .Take(pageSize)
-            .Select(x => new AppointmentWithDetailsDto(
-                x.a.Id, x.a.PatientUserId, x.a.DoctorUserId, x.a.StartUtc, x.a.EndUtc,
-                x.a.Status, x.a.Notes, x.du.FirstName, x.du.LastName, x.dp.NmcNumber,
-                x.du.ProfilePhotoUrl, x.dp.Specialization, x.pu.FirstName, x.pu.LastName,
-                x.pp != null ? x.pp.Sex : Sex.Unknown,
-                x.pp != null ? CalculateAge(x.pp.DateOfBirth, today) : 0,
-                x.pu.ProfilePhotoUrl
-            ))
-            .ToListAsync(cancellationToken);
-
-        return new PagedResult<AppointmentWithDetailsDto>(items, totalCount, page, pageSize);
-    }
-
-    public async Task<PagedResult<AppointmentWithDetailsDto>> GetForDoctorWithDetailsAsync(
+    public Task<PagedResult<AppointmentWithDetailsDto>> GetForDoctorWithDetailsAsync(
         Guid doctorUserId,
+        int page,
+        int pageSize,
+        CancellationToken cancellationToken)
+        => GetWithDetailsAsync(a => a.DoctorUserId == doctorUserId, page, pageSize, cancellationToken);
+
+    private async Task<PagedResult<AppointmentWithDetailsDto>> GetWithDetailsAsync(
+        Expression<Func<Appointment, bool>> predicate,
         int page,
         int pageSize,
         CancellationToken cancellationToken)
     {
         var today = DateOnly.FromDateTime(DateTime.UtcNow);
 
-        // 1. Get the total count
-        var baseQuery = _db.Appointments.Where(a => a.DoctorUserId == doctorUserId);
+        var baseQuery = _db.Appointments.AsNoTracking().Where(predicate);
+
         var totalCount = await baseQuery.CountAsync(cancellationToken);
 
-        // 2. Perform the Joins
         var query = baseQuery
-            .AsNoTracking()
             .Join(_db.Users.AsNoTracking(), a => a.DoctorUserId, u => u.Id, (a, du) => new { a, du })
             .Join(_db.DoctorProfiles.AsNoTracking(), x => x.a.DoctorUserId, dp => dp.UserId, (x, dp) => new { x.a, x.du, dp })
             .Join(_db.Users.AsNoTracking(), x => x.a.PatientUserId, pu => pu.Id, (x, pu) => new { x.a, x.du, x.dp, pu })
             .LeftJoin(_db.PatientProfiles.AsNoTracking(), x => x.a.PatientUserId, pp => pp.UserId, (x, pp) => new { x.a, x.du, x.dp, x.pu, pp });
 
-        // 3. Paginate and Select
         var items = await query
             .OrderByDescending(x => x.a.StartUtc)
             .Skip((page - 1) * pageSize)
