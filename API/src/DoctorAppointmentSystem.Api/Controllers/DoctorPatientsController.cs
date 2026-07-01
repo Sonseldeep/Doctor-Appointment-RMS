24db45fd59d59df0df78a3aadaf00ad802aec0fa
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-07-01 11:22:04 +0545

    remove refactor the code and ungroup the problme and errors

--- API/src/DoctorAppointmentSystem.Api/Controllers/DoctorPatientsController.cs
+++ API/src/DoctorAppointmentSystem.Api/Controllers/DoctorPatientsController.cs
@@ -1,6 +1,6 @@
 ﻿using DoctorAppointmentSystem.Application.Features.Doctors.Patients.GetPatientRecords;
 using DoctorAppointmentSystem.Application.Features.Doctors.Patients.SearchPatients;
-using MediatR; // 1. Added MediatR namespace
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,10 +10,8 @@ namespace DoctorAppointmentSystem.Api.Controllers;
 [Route("api/doctor/patients")]
 public class DoctorPatientsController : ApiController
 {
-    // 2. Add a private field for the sender
     private readonly ISender _sender;
 
-    // 3. Inject ISender via the constructor
     public DoctorPatientsController(ISender sender)
     {
         _sender = sender;
@@ -24,11 +22,10 @@ public class DoctorPatientsController : ApiController
     {
         var query = new SearchPatientsQuery(q);
 
-        // 4. Use _sender instead of Sender
         var result = await _sender.Send(query);
 
         return result.Match(
-            patients => Ok(patients),
+            Ok,
             errors => Problem(errors.ToString())
         );
     }
@@ -38,11 +35,10 @@ public class DoctorPatientsController : ApiController
     {
         var query = new GetPatientRecordsForDoctorQuery(patientId);
 
-        // 5. Use _sender instead of Sender
         var result = await _sender.Send(query);
 
         return result.Match(
-            reports => Ok(reports),
+            Ok,
             errors => Problem(errors.ToString())
         );
     }
