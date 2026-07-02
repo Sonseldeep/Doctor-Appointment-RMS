1a2d8e0aaea52f4f2158604ea9b58e018d5fec15
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-07-02 11:20:41 +0545

    add securityOptions

---
+++ API/src/DoctorAppointmentSystem.Infrastructure/Abstractions/Authentication/SecurityOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace DoctorAppointmentSystem.Infrastructure.Abstractions.Authentication;
+
+public sealed class SecurityOptions
+{
+    public const string SectionName = "Security";
+
+    public int MaxFailedLoginAttempts { get; init; } = 5;
+    public int LockoutDurationHours { get; init; } = 24;
+    public int PasswordExpiryDays { get; init; } = 60;
+}
